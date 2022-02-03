e9277af2ae0d6f34ac5dbf65c326c25c216578fe
Саргсян Юра Араикович <skullkon@gmail.com>
2022-02-03 17:07:20 +0700

    added first endpoints and storage like a list

---
+++ WebApplication1/WebApplication1/Program.cs
@@ -0,0 +1,53 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+/*GET api/v1/persons
+Возвращает массив объектов типа Person:
+[Person, Person, …]
+
+GET api/v1/person/[id]
+Где id – уникальный идентификатор сотрудника.
+Возвращает объект типа Person.
+
+POST api/v1/person
+Где id – уникальный идентификатор сотрудника.
+В теле запроса передавать объект Person. Id должен быть null или undefined.
+Создаёт нового сотрудника в системе с указанными навыками.
+
+PUT api/v1/person/[id]
+Где id – уникальный идентификатор сотрудника.
+В теле запроса передавать объект Person. Id должен быть null или undefined.
+Обновляет данные сотрудника согласно значениям, указанным в объекте Person в теле. Обновляет навыки сотрудника согласно указанному набору.
+
+DELETE api/v1/person/[id]
+Где id – уникальный идентификатор сотрудника.
+Удаляет с указанным id сотрудника из системы.
+
+*/
+
+var persons = new List<Person>();
+persons.Add(new Person() {Name = "Sergey", DisplayName = "Sergey", Id = 1});
+persons[0].Skills.Add(new Skill(){Id = 10,Level = 10,Name = "test"});
+
+app.MapGet("/api/v1/persons", () => persons);
+app.MapGet("/api/v1/person/{id}", (int id) => persons.FirstOrDefault(p => p.Id == id));
+app.MapPost("/api/v1/person", (Person person) => persons.Add(person));
+// app.MapPut("/api/v1/person/{id}", () => { });
+// app.MapDelete("/api/v1/person/{id}", () => { });
+
+app.Run();
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+    public List<Skill> Skills { get; set; } = new List<Skill>();
+}
+
+public class Skill
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public byte Level { get; set; }
+}
